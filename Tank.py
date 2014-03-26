be174c7540910fddad984a1aa21d07a5edd09c5f
Brandon Hayduk <bmhayduk@ualberta.ca>
2014-03-26 14:01:50 -0600

    Updated tank

--- Tank.py
+++ Tank.py
@@ -14,15 +14,26 @@ class Tank():
 
 		self._hp = TANK_MAXHP
 		self._alive = True 
+		
+		
 
-	def move_tank(self, new_position):
+	def move_tank(self, distance):
 		"""
 		This method moves the tank to a new position
 		"""
+		self._position[0] += distance
+		
+
+	def is_hit(self, location)
+		if(abs(shot[0]-position[0]) <10):
+			if(abs(shot[1]-self._position[1]) < 10)
+				return True
+		
+		
 
 	def take_damage(self, damage):
 		"""
 		Removes HP from the tank
 		"""
-
+		
 		self._hp -= damage
