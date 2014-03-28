701e3bdc824c8c5d5731f60b2d2f0f0546da77cb
paulomoreno <paulomorenoo@gmail.com>
2014-03-28 16:52:03 -0600

    Working simples interface

--- Interface.py
+++ Interface.py
@@ -1,7 +1,24 @@
 import pygame, sys
 
 class Interface():
-	def __init__(self, resolution, color):
-		self.resolution = resolution
-		windowSurfaceObj = pygame.display.set_mode(resolution)
-		windowSurfaceObj.fill(color)
+	"""
+	"""
+	def __init__(self, level, bg_color, windowSurfaceObj, pygame):
+
+		self._level = level
+		self._pygame = pygame
+		self._bg_color = bg_color
+		self._loadLevel()
+
+		self._windowSurfaceObj = windowSurfaceObj
+		self._windowSurfaceObj.fill(bg_color)
+
+	def _loadLevel(self):
+
+		#self._resolution = (1280,600)
+		pass
+
+
+	def update(self):
+
+		self._pygame.display.update()
--- TankWars.py
+++ TankWars.py
@@ -1,39 +1,34 @@
-import sys, pygame, tiles
-from gui import GUI
+import pygame, sys
+from pygame.locals import *
+from Interface import Interface
 
-#Defaut values
-RESOLUTION = pygame.Rect(0, 0, 800, 600)
-#The default color is a blue color - the sky
-BG_COLOR = (125, 165, 255)
+# Constants
+SCREEN_SIZE = (1280, 720)
+BG_COLOR = (50, 100, 255)
 
-# Initialize everything
-pygame.mixer.pre_init(22050, -16, 2, 512) # Small buffer for less sound lag
+
+# Initialization
 pygame.init()
-pygame.display.set_caption("Tank Wars")
-main_gui = GUI(RESOLUTION, BG_COLOR)
-clock = pygame.time.Clock()
-argv = sys.argv[1:]
+fpsClock = pygame.time.Clock()
 
-# If a filename was given, load that level. Otherwise, load a default.
+windowSurfaceObj = pygame.display.set_mode((1280,720))
+windowSurfaceObj.fill(BG_COLOR)
+
+# Our main interface 
+# Right now it uses the level 1
+# 	TODO: make a code to read from the command line arguments 
+# 		  and set the map as the command line arg
 level = "1"
-if len(argv) > 0:
-    level = argv[0]
-#main_gui.load_level("maps/" + level + ".lvl")
+main_interface = Interface(level, BG_COLOR, windowSurfaceObj, pygame)
 
-# The main game loop
-while 1:
+pygame.display.set_caption('Tank Wars')
+
+while True:
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.display.quit()
-            sys.exit()
-        # End if q is pressed
-        elif (event.type == pygame.KEYDOWN and
-        (event.key == pygame.K_q or event.key == pygame.K_ESCAPE)):
-            pygame.display.quit()
-            sys.exit()
-        # Respond to clicks
-        elif event.type == pygame.MOUSEBUTTONUP:
-            main_gui.on_click(event)
-    main_gui.update()
-    main_gui.draw()
-    clock.tick(60)
+        if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+    main_interface.update()
+    fpsClock.tick(60)
