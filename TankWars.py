281df165a7c450a2f25acd004d98382393f1223f
paulomoreno <paulomorenoo@gmail.com>
2014-03-28 19:02:59 -0600

    Now we read from the map and there's also an test of shot hitting the mountain

--- TankWars.py
+++ TankWars.py
@@ -1,6 +1,6 @@
 import pygame, sys
 from pygame.locals import *
-from Interface import Interface
+from interface import Interface
 
 # Constants
 SCREEN_SIZE = (1280, 720)
@@ -11,15 +11,12 @@ BG_COLOR = (50, 100, 255)
 pygame.init()
 fpsClock = pygame.time.Clock()
 
-windowSurfaceObj = pygame.display.set_mode((1280,720))
-windowSurfaceObj.fill(BG_COLOR)
-
 # Our main interface 
 # Right now it uses the level 1
 # 	TODO: make a code to read from the command line arguments 
 # 		  and set the map as the command line arg
 level = "1"
-main_interface = Interface(level, BG_COLOR, windowSurfaceObj, pygame)
+main_interface = Interface(level, BG_COLOR, pygame)
 
 pygame.display.set_caption('Tank Wars')
 
---
+++ maps.py
@@ -0,0 +1,83 @@
+import pygame
+
+
+MOUNTAIN_COLOR = (50,255,50)
+
+PIXEL_SKY = 0
+PIXEL_MOUNTAIN = 1
+
+EXPLOSION_RADIUS = 25
+
+class Map():
+    """
+    This class represents the map (including all graphics/tiles information)
+    """
+    def __init__(self, level, bg_color):
+        """
+        Initialize all the map information and functionality
+        """
+        # Initialize all necessary information
+        self._mapName = level + '.gif'
+        self._bg_color = bg_color
+        self._pixels = dict()
+
+        # Load the .gif map file
+        self._loadMap()
+
+    def _loadMap(self):
+        """
+        Load the .gif file and set the pixels dictionary accordingly
+        """
+        #gets the filename
+        filename = 'maps/' + self._mapName
+
+        # Load in the map image.
+        map_image = pygame.image.load(filename)
+        self._map_width, self._map_height = map_image.get_size()
+
+        # Go through the image adding pixels
+        for w in range(map_image.get_width()):
+            for h in range(map_image.get_height()):
+
+                # If this pixel is green, this is mountain
+                if map_image.get_palette_at(map_image.get_at_mapped((w, h))).g > 250:
+                    self._pixels[(w,h)] = PIXEL_MOUNTAIN
+                # Otherwise, this is sky
+                else:
+                    self._pixels[(w,h)] = PIXEL_SKY
+
+
+    def paintMountain(self, windowSurfaceObj):
+        """
+        Paint the mountain according to the list of pixels
+        """
+        for pos in self._pixels:
+            if self._pixels[pos] == PIXEL_MOUNTAIN:
+                windowSurfaceObj.set_at(pos, MOUNTAIN_COLOR)
+
+    def didShotHitMountain(self, shot_position, power, windowSurfaceObj):
+        """
+        Check if a given shot hitted the mountain. 
+            If it did, return True and destroy the mountain according to
+            a explosion radius
+        """
+
+        #If the pixel hitted is a mountain:
+        if self._pixels[shot_position] == PIXEL_MOUNTAIN:
+            radius = EXPLOSION_RADIUS * power
+
+            # Run trhough a "kind of" cicrcle shape in order to
+            # remove these pixels as mountain and set them as
+            # sky pixels.
+            for x in range(shot_position[0]-radius,shot_position[0]+radius):
+                for y in range(shot_position[1]-radius,shot_position[1]+radius):
+                    # Check if its close to the radio
+                    if abs(x-shot_position[0])+abs(y-shot_position[1]) < 1.3*radius:
+                        # Check if this is a mountain pixel
+                        if self._pixels[(x,y)] == PIXEL_MOUNTAIN:
+                            self._pixels[(x,y)] = PIXEL_SKY
+                            windowSurfaceObj.set_at((x,y), self._bg_color)
+            return True
+        else:
+            return False
+
